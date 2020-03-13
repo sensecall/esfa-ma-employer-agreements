9ef61f9edc76a998ea94517c4d3ac84528d37ed2
Dan Sensecall <dan@sensecall.co.uk>
2020-03-13 12:08:47 +0000

    names

--- app/views/3-0/routes.js
+++ app/views/3-0/routes.js
@@ -10,5 +10,9 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/config', (req, res, next) => {
+	let businessCount = req.session.data['orgs-count']
+	let allBusinessesCount = req.session.data['businesses'].length
+
+	req.session.data['random-businesses'] = Array.from({length: businessCount}, () => Math.floor(Math.random() * allBusinessesCount)).sort((a, b) => a - b);
 	res.redirect('organisations')
 })
